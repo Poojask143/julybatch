8a14d3380aca7d2aaa4b4c5a7c96dc941bd6db8c
poojask143 <poojasurwase011@gmail.com>
2023-12-01 13:26:49 +0530

    html code add

---
+++ javascript/variables.js
@@ -0,0 +1,14 @@
+//variable :are the container to store data (data type)
+//let/var/const default var(keywords)fallowed by variable name
+//variable name :rule>>> space not allowed ,first letter of variable should always alphabets
+//spe
+
+letfName;//declaration of variable//undefined
+fName='priya';//defination(syntax variableName =value/data)//difference= == ===
+
+let myFrist_Name='pooja';
+
+var mySName='surwase';
+
+var myName;
+myName='om'
