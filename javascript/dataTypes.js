3adc7cc7aad19721dcf609d34c7fb7db5a1f75b6
poojask143 <poojasurwase011@gmail.com>
2023-12-02 20:36:54 +0530

    js code add

--- javascript/dataTypes.js
+++ javascript/dataTypes.js
@@ -18,7 +18,7 @@ let city="mumbai";
 
 console.log('city1',city);
 city='pune';
-console.log('city2';city);
+console.log('city2',city);
 
  var marks=90; //number datatypoe
  console;log('marks',marks);
@@ -29,8 +29,60 @@ console.log('city2';city);
 varisFavFruit=false;//boolean true/false;
 console.log('data types of isFavFruit var',typeof(isFavFruit));
 
-cont match=undefined;
+const match=undefined;
 console.log(match);
 
+let abc;
+console.log(abc);
+console.log(typeof(abc));
+
+var myName=null;
+console.log(myName);
+console.log(myName);
+
+let a=656787866767687;
+console.log(a); 
+
+//let b=5.3456789 >>5.34;
 
 
+//let c ="999999999999999";
+//console.log(BigInt(c));
+
+let d=999999999999999n
+console.log(d);
+
+let k=null;
+
+//array: is used to store multiple items
+let name="pooja live in mumbai,mumbai is big city"
+console.log(name);
+
+let cities=[]; //empty array
+
+cities=["pune","mumbai","naded","parli","beed"];  //homogeneous array
+console.log(typeof(cities));   //array data type is always object
+comsole.log(cities[4]);//pune
+console.log(cities[10]);//undefined
+console.log(cities.length);
+comsole.log(cities[0]);//parli
+console.log(cities[cities.length-1]);//beed
+
+let hetroArray=['poonam',505000,test(),true,null,undefined,'pooja']
+console.log(hetroArray[2]);
+
+function test(){
+    //return 'poonam';
+
+}
+
+let cars={
+    carName:"swift",
+    carPrice:800000,
+    carColor:"white",
+    carNumber:MH-44(5312),
+}
+
+console.log(cars.carName);
+console.log(cars.carColor);
+console.log(typeof(cars));
---
+++ javascript/operators.js
@@ -0,0 +1,32 @@
+// =>> assignment operator  --to assign vallue to the variable.
+// == >> equality operator, --to compare value of two variable,
+//=== >>equality operator -- it will compare value as well as data 
+
+let a=40; //assignment operator's e.g.
+
+let b="40"
+
+console.log(a ==b ); //it will strickly check value and DT.
+
+console.log( a ===b);
+
+let c =20;
+var d =30;
+
+
+console.log(c != d);// ! not operator !true >> false >> true
+
+console.log( c ==d);//fasle
+
+let isFav =true;
+
+console.log(isFav);//false 
+
+let e=40;
+let f=50;
+console.log(">" ,e < f); //false it will ckeck only for values
+console.log("<", e < f); //true 
+
+console.log("<=", e <= f);//less than equal to operator
+
+
