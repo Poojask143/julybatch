6f5312bcc98311458c499a160a5dab553f7e608b
poojask143 <poojasurwase011@gmail.com>
2023-12-02 13:37:54 +0530

    js code add

--- javascript/dataTypes.js
+++ javascript/dataTypes.js
@@ -14,8 +14,23 @@
 //an array
 //a date
 console.log('khjjhh');console.log('pooja');
+let city="mumbai";
 
-console.log(city);
+console.log('city1',city);
 city='pune';
-console.log(city);
+console.log('city2';city);
+
+ var marks=90; //number datatypoe
+ console;log('marks',marks);
+
+ var mobNo=8765678765; //string  
+ console.log('mobNo',mobNo);
+
+varisFavFruit=false;//boolean true/false;
+console.log('data types of isFavFruit var',typeof(isFavFruit));
+
+cont match=undefined;
+console.log(match);
+
+
 
