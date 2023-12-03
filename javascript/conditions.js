e4b20be85f8336307f01eaf8de0e6856309ea303
poojask143 <poojasurwase011@gmail.com>
2023-12-03 13:49:13 +0530

    js code add

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -1,28 +1,33 @@
 //if condition ,if else, else if,switch
-
 let items = 40;
-
 // if(){}
 //code block
-
 //}
+
 //items greater than or equal to 50
 if (items >=50){
-console.log("stock is available");
+    document.write("stock is available");
 }
+
 // if else
 if (items >=50){
-    console.log("limited is available");
-}
- //else if :multiple cond 
- else if (items >=20){
- console.log("few items left");
-
- }
-
- else if (items >=10){
-    console.log(" hurry up ! few stock available");
+    document.write("stock is available");
 }
 else{
-    console.log("out of stock");
+    document.write("limited is available");
+    }
+
+ //else if :multiple cond 
+ if (items >=50){
+    document.write("stock is available");
+    }
+ if(items >=20 && items <=50){
+    document.write("few items left");
+
+ }
+ else if (items >=1 && items <=20){
+    document.write(" hurry up ! few stock available");
+}
+else{
+    document.write("out of stock");
 }
--- javascript/table.js
+++ javascript/table.js
@@ -2,10 +2,9 @@ function addNumber(){
     let number = parseInt(prompt("Enter the number"));
     for(let i = 1;i <=10; i++) {
         let res = number * 1;
-        console.log(number);
-        document.write(res,'\n');
-
-        //document.getElementById("table").innerHTML=res +'<br>'
-
+        console.log(res);
+        document.write(res,'<br>');
     }
+    document.getElementById("table").innerHTML=res +'<br>'
+
 }
